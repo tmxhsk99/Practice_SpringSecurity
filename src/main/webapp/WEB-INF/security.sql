5262c7c99f069455b6c1b0b672746f398d51f1e8
kimjuhyeon <tmxhsk99@naver.com>
2019-01-11 10:39:23 +0900

    인증권한 테이블 sql 추가

--- src/main/webapp/WEB-INF/security.sql
+++ src/main/webapp/WEB-INF/security.sql
@@ -25,3 +25,20 @@ insert into authorities (username,authority) values('admin00','ROLE_ADMIN');
 commit;
 
 select * from users;
+
+
+--인증 / 권한을 위한 테이블설계 기존의 DB를 사용하는 케이스 
+create table tbl_member(
+	userid varchar2(50) not null primary key,
+	userpw varchar2(100) not null,
+	username varchar2(100) not null,
+	regdate date default sysdate,
+	updatedate date default sysdate,
+	enabled char(1) default '1');
+
+create table tbl_member_auth(
+	userid varchar2(50) not null,
+	auth varchar2(50) not null ,
+	constraint fk_member_auth foreign key(userid) references
+	tbl_member(userid) 
+);
